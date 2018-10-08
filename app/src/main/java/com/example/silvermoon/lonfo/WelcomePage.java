f2ae0fa356ec090930d176fa5b3614c9f1886e95
mephi007 <roysumit949@gmail.com>
2018-10-08 18:42:24 +0530

    added splash screen

--- app/src/main/java/com/example/silvermoon/lonfo/WelcomePage.java
+++ app/src/main/java/com/example/silvermoon/lonfo/WelcomePage.java
@@ -4,32 +4,33 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
 public class WelcomePage extends AppCompatActivity {
 
-    public void init()
-    {
-        Button lost_button = (Button) findViewById(R.id.lost_button);
-        Button found_button = (Button) findViewById(R.id.found_button);
-
-        lost_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent activity2 = new Intent(WelcomePage.this, Activity2.class);
-                startActivity(activity2);
-            }
-        });
-
-        found_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent activity2 = new Intent(WelcomePage.this, Activity2.class);
-                startActivity(activity2);
-
-            }
-        });
-    }
+//    public void init()
+//    {
+//        Button lost_button = (Button) findViewById(R.id.lost_button);
+//        Button found_button = (Button) findViewById(R.id.found_button);
+//
+//        lost_button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent activity2 = new Intent(WelcomePage.this, Activity2.class);
+//                startActivity(activity2);
+//            }
+//        });
+//
+//        found_button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent activity2 = new Intent(WelcomePage.this, Activity2.class);
+//                startActivity(activity2);
+//
+//            }
+//        });
+//    }
 
 
 
@@ -37,6 +38,25 @@ public class WelcomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_page);
-        init();
+//       init();
+
+        LogoLauncher logoLauncher = new LogoLauncher();
+        logoLauncher.start();
+    }
+
+    private class LogoLauncher extends Thread{
+
+        public void run(){
+            try{
+                sleep(3000);
+            }
+            catch(InterruptedException e){
+                e.printStackTrace();
+            }
+
+            Intent intent = new Intent(WelcomePage.this , LoginActivity.class);
+            startActivity(intent);
+            WelcomePage.this.finish();
+            }
     }
 }
