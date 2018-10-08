4c4801005d2b5263a66754be9c50899120bd6b61
mephi007 <roysumit949@gmail.com>
2018-10-09 01:44:13 +0530

    Text fields working same as Ujjwals instruction and made register_here clickable, proceeding with registration page now

--- app/src/main/java/com/example/silvermoon/lonfo/LoginActivity.java
+++ app/src/main/java/com/example/silvermoon/lonfo/LoginActivity.java
@@ -1,13 +1,44 @@
 package com.example.silvermoon.lonfo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+//        public void init()
+//    {
+//        Button lost_button = (Button) findViewById(R.id.lost_button);
+//        Button found_button = (Button) findViewById(R.id.found_button);
+//
+//        lost_button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent activity2 = new Intent(WelcomePage.this, Activity2.class);
+//                startActivity(activity2);
+//            }
+//        });
+              public void init()
+              {
+                  TextView register = (TextView) findViewById(R.id.Register_here);
+                  register.setOnClickListener(new View.OnClickListener() {
+                      @Override
+                      public void onClick(View view) {
+
+                          Intent register_page = new Intent(LoginActivity.this , RegisterActivity.class);
+                          startActivity(register_page);
+
+                      }
+                  });
+
+              }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        init();
     }
 }
